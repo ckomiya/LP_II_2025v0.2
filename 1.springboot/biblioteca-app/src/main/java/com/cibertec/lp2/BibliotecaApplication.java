442c8a2bc2ed488599c7a678b2f1a5a4e28dc90c
ckomiya <christian.komiya@gmail.com>
2025-05-14 19:47:15 -0500

    Renombrar clase principal a BibliotecaApplication y agregar HelloController

--- 1.springboot/biblioteca-app/src/main/java/com/cibertec/lp2/BibliotecaApplication.java
+++ 1.springboot/biblioteca-app/src/main/java/com/cibertec/lp2/BibliotecaApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BibliotecaAppApplication {
+public class BibliotecaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BibliotecaAppApplication.class, args);
+		SpringApplication.run(BibliotecaApplication.class, args);
 	}
 
 }
